02f4ddb5c11f8b9fa591b7d46356ef50b0f55f8e
Steve Bowser <stevewbowser@gmail.com>
2021-01-01 22:40:22 +0000

    added chart for every finance type

---
+++ FinancialAdvisorAppUI/FinanceCalculations/financialstatshelper.cs
@@ -0,0 +1,22 @@
+﻿using FinancialAdvisorAppUI.Models.Ref;
+using FinancialAdvisorAppUI.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialAdvisorAppUI.FinanceCalculations
+{
+    public static class financialstatshelper
+    {
+        public static Dictionary<FinanceType, List<FinancialStat>> SplitOutFinancialStatsByCategory(List<FinanceType> financeTypes, List<FinancialStat> finStats)
+        {
+            Dictionary<FinanceType, List<FinancialStat>> brokenDownFinanceStats = new Dictionary<FinanceType, List<FinancialStat>>();
+            foreach (var finType in financeTypes)
+            {
+                brokenDownFinanceStats.Add(finType, finStats.Where(f => f.FinanceTypeId == finType.Id).ToList());
+            }
+            return brokenDownFinanceStats;
+        }
+    }
+}
